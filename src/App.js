bbfb58fd0afb73c87d8199d8c7ecc227f7afef6f
Jon Bernal <jbernalwrc@gmail.com>
2021-07-09 23:10:22 -0700

    did quick scaffold of site and layout.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { About } from "./pages/About";
+
+import styles from "./App.module.css";
+import { Work } from "./pages/Work";
+import { Skills } from "./pages/Skills";
+import { Contact } from "./pages/Contact";
 
 function App() {
+  const [openApp, setOpenApp] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.iconGroup}>
+        <button onClick={() => setOpenApp("about")} className={styles.icon}>
+          About Logo
+        </button>
+        <button onClick={() => setOpenApp("work")} className={styles.icon}>
+          Work History Logo
+        </button>
+        <button onClick={() => setOpenApp("skills")} className={styles.icon}>
+          Skills Logo
+        </button>
+        <button onClick={() => setOpenApp("contact")} className={styles.icon}>
+          contact Logo
+        </button>
+      </div>
+
+      {!!openApp && (
+        <div className={styles.appWindow}>
+          <button className={styles.closeBtn} onClick={() => setOpenApp("")}>
+            X
+          </button>
+          {openApp === "about" && <About />}
+          {openApp === "work" && <Work />}
+          {openApp === "skills" && <Skills />}
+          {openApp === "contact" && <Contact />}
+        </div>
+      )}
     </div>
   );
 }
