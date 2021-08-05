f8731528bd03541604f3d6c9590dac36b18f9dd5
jon Bernal <jbernalwrc@gmail.com>
2021-08-04 22:35:38 -0700

    adjusted so it will publish properly

--- src/pages/Portfolio/portfolioData.js
+++ src/pages/Portfolio/portfolioData.js
@@ -59,8 +59,8 @@ const tech = {
   },
 };
 
-const html =
-  "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg";
+// const html =
+//   "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg";
 // const css = "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
 
 export const portfolioData = [
