ba1d2380041586b0b8429e429c5cedbb5352ad68
jon Bernal <jbernalwrc@gmail.com>
2023-09-27 17:43:14 -0700

    update about me info.

--- src/pages/Apple/AppWindow/About/About.jsx
+++ src/pages/Apple/AppWindow/About/About.jsx
@@ -18,20 +18,17 @@ const About = () => {
           <span style={{ fontWeight: "bold" }}>Jon</span> Bernal
         </h1>
         <p>
-          I grew up in Northern Virginia and now live in Washington State as a
-          Software Engineer at{" "}
-          <a className={styles.link} href="https://nowigence.com">
-            Nowigence Inc
-          </a>
-          . I have significant experience with Web technologies, building and
-          securing APIs, developing performant and scalable backends, and
-          putting together frontends to give users a phenomenal user experience.
+          I grew up in Northern Virginia and now live on Whidbey Island in
+          Washington State as a Senior Software Engineer. I have significant
+          experience with Web technologies, building and securing APIs,
+          developing performant and scalable backends, and putting together
+          frontends to give users a phenomenal user experience.
         </p>
         <p>
-          In my spare time, I enjoy reading, rock climbing, hiking, playing with
-          new linux distros and more CODING! When not hard at work, you'll find
-          me spending time with my wife and three rabbits, or happily lost in
-          the woods somewhere.
+          In my spare time, I enjoy wood working, reading, rock climbing,
+          hiking, playing with new linux distros and more CODING! When not hard
+          at work, you'll find me spending time with my wife and three rabbits,
+          or happily lost in the woods somewhere.
         </p>
       </div>
     </div>
